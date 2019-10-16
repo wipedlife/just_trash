04894c0b3eed3d82f0876cb3c14ef248a34ec292
wipedlife <gigoga367@gmail.com>
2019-10-16 10:44:48 +0700

    fix size

--- myHash.py
+++ myHash.py
@@ -45,15 +45,41 @@ class myHash:
  def C(self, str):
    v = 0
    for s in str:
-       e=self.Z(ord(s))
-       c = (v << 8 | e)
-       v = ( v + (v ^ c) ) | (14+12)
-   return (v)
- def Z(self,N):
+       e=self.W(ord(s))
+       c = (v << 8 | e) << (14+12) | (14+12)
+       v = ( (v ^ c)^e ) << (14+12) | (14+12)
+   return ( (v << (14+12)) ^ (14+12) )
+ def B0(self, N):
+     return [((N & (0xff << pos*8)) >> pos*8 )^7 for pos in range(14+12)]
+ def B1(self, N):
+     return [((N & (0xff << pos*8)) >> pos*8 )^2 for pos in reversed( range(14+12) )]
+ def B2(self, N):
+  N = N << 14
+  return [((N & (0xff << pos*8)) >> pos*8 )^N for pos in range(14+12)]
+
+ def W(self,N):
   a1 = self.A1(N)
   a2 = self.A2(N)
   a3 = self.A3(N)
-  return ((a1+a2+a3)^self.A4(N) << (14+12) ) | (14+12)
+  rv= ((a1+a2+a3)^self.A4(N) << (14+12) ) ^ (14+12)
+  return rv
+
+ #TODO: fix much of collisions
+ def Z(self, N):
+   b0=self.B0(N)
+   b1=self.B1(N)
+   b2=self.B2(N)
+   b = 0
+   d = 0
+   e = 0
+   for num in b0:
+       b = (b << 8) | num
+   for num in b1:
+       d = (d << 8) | num
+   for num in b2:
+       e = (e << 8) | num
+   return self.A1(b^d & e)
+     
  def strToHex(self,str):
      num = self.Z(self.C( str ))
      print ( num )
