bd12eb5392b18a96b8010c6e45e74add32395070
wipedlife <gigoga367@gmail.com>
2019-10-17 19:00:13 +0700

    fix init blocks

--- myHash_.c
+++ myHash_.c
@@ -3,7 +3,7 @@
 hash_block * init_data(void * ptr, size_t s){
 
 	ubyte * sP = ptr;
-	size_t count_blocks = 1+(s / MAGICNUMBER);	
+	size_t count_blocks = 1+(s / MAGICNUMBER) ;	
 	printf("Size %lu ; %lu \n", s, count_blocks);
 
 	hash_block * bytes = calloc( sizeof(hash_block) ,
@@ -15,13 +15,12 @@ hash_block * init_data(void * ptr, size_t s){
 
 		for(char i = MAGICNUMBER; i >= 0; i--)
 		       (bytes[cb]).data[i]=0;	
-
+		printf("%llu %llu %llu %p\n", count_blocks, cb, (cb*MAGICNUMBER), sP);
 		memcpy( (bytes[cb]).data, sP,
-				cb == 0 ? MAGICNUMBER : (cb*MAGICNUMBER) 
+				 MAGICNUMBER  
 				);
 		printf("data now: %s\n", bytes[cb].data);
-		sP+=
-			cb == 0 ? MAGICNUMBER : (cb*MAGICNUMBER);
+		sP+=MAGICNUMBER ;
 		bytes[cb].num=1+(cb);
 	}
 	return bytes;;
